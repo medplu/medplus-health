fbc732c76017dd35091e47e88d8f5a68fbb26ede
Your Name <your_email@example.com>
2024-10-08 21:17:48 +0300

    modified the paystack component

--- components/PaystackPayment.tsx
+++ components/PaystackPayment.tsx
@@ -1,29 +1,26 @@
+
 import React from 'react';
-import { Paystack } from 'react-native-paystack-webview';
+import  { Paystack }  from 'react-native-paystack-webview';
 import { View } from 'react-native';
 
-interface PaystackPaymentProps {
-  amount: number;
-  billingEmail: string;
-  onSuccess: (res: any) => void;
-  onError: (e: any) => void;
-}
-
-const PaystackPayment: React.FC<PaystackPaymentProps> = ({ amount, billingEmail, onSuccess, onError }) => {
+export default function PaystackPayment() {
   return (
     <View style={{ flex: 1 }}>
-      <Paystack
+      <Paystack  
         paystackKey="pk_test_81ffccf3c88b1a2586f456c73718cfd715ff02b0"
-        amount={amount}
-        billingEmail={billingEmail}
-        onCancel={(e) => onError(e)}
-        onSuccess={(res) => onSuccess(res)}
+        amount={25000.00}        
+        billingEmail="medpluscollaborate@gmail.com"
+        
         currency='KES'
         activityIndicatorColor="green"
+        onCancel={(e) => {
+          
+        }}
+        onSuccess={(res) => {
+          
+        }}
         autoStart={true}
       />
     </View>
   );
-};
-
-export default PaystackPayment;
+}
