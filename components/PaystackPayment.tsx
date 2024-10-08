5fd8c4d098212f71c2d4cfc60b7a270b8c1b9bbe
Your Name <your_email@example.com>
2024-10-08 16:55:10 +0300

    modified the paystack component

--- components/PaystackPayment.tsx
+++ components/PaystackPayment.tsx
@@ -22,7 +22,7 @@ export default function PaystackPayment() {
             paystackKey="pk_test_81ffccf3c88b1a2586f456c73718cfd715ff02b0"
             billingEmail="medpluscollaborate@gmail.com"
             amount={25000.00}
-            currency='NGN'
+            currency="NGN"
             onCancel={(e) => {
               console.log('Payment Cancelled:', e);
               setModalVisible(false);
