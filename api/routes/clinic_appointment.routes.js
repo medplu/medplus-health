7a03eebf92e3a4fe56b58bb8fb41269057da0b19
Your Name <your_email@example.com>
2024-10-10 10:51:32 +0300

    fixed issues with the clinic_appointment routes

--- api/routes/clinic_appointment.routes.js
+++ api/routes/clinic_appointment.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const clinicAppointmentController = require('../controllers/appointment.controller');
+const clinicAppointmentController = require('../controllers/clinic_appointment.controller');
 
 // Route to create a new appointment
 router.post('/appointments', clinicAppointmentController.createAppointment);
