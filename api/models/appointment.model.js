58ad24d3b06f514ae007b686b58cc8846f91a276
Your Name <your_email@example.com>
2024-10-14 20:46:24 +0300

    modified the booking model

--- api/models/appointment.model.js
+++ api/models/appointment.model.js
@@ -25,7 +25,7 @@ const appointmentSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ['booked', 'confirmed', 'completed', 'cancelled'], // Added 'confirmed'
+    enum: ['pending', 'booked', 'confirmed', 'completed', 'cancelled'], // Added 'pending'
     default: 'booked',
   },
 }, {
