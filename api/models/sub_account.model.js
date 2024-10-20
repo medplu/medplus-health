a33aaba614d87e58db25338c27249d3558018d8c
Your Name <your_email@example.com>
2024-10-20 14:35:56 +0300

    modified the models and controllers

---
+++ api/models/sub_account.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const SubaccountSchema = new mongoose.Schema({
+    business_name: { type: String, required: true },
+    account_number: { type: String, required: true },
+    percentage_charge: { type: Number, required: true },
+    settlement_bank: { type: String, required: true },
+    currency: { type: String, required: true },
+    subaccount_code: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the User model
+}, { timestamps: true });
+
+const Subaccount = mongoose.model('Subaccount', SubaccountSchema);
+module.exports = Subaccount;
