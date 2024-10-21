af8e6c89675a8eae55493cf715195e3035f31ae0
Your Name <your_email@example.com>
2024-10-21 09:18:28 +0300

    modified the pharmacy routes

--- api/routes/pharmacy.routes.js
+++ api/routes/pharmacy.routes.js
@@ -6,9 +6,9 @@ const router = express.Router();
 // Route to create a pharmacy (only pharmacists can create pharmacies)
 router.post('/api/pharmacies', createPharmacy);
 
-// Additional routes for pharmacy management can be added here in the future
- router.get('/api/pharmacies/:id', getPharmacyById);
- router.put('/api/pharmacies/:id', updatePharmacy);
- router.delete('/api/pharmacies/:id', deletePharmacy);
+
+//  router.get('/api/pharmacies/:id', getPharmacyById);
+//  router.put('/api/pharmacies/:id', updatePharmacy);
+//  router.delete('/api/pharmacies/:id', deletePharmacy);
 
 module.exports = router;
