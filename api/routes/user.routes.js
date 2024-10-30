5431c9c26645bafbbf6221b8a8d6006f2681cdaf
Your Name <your_email@example.com>
2024-10-30 23:19:57 +0300

    modified the user routes

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -6,18 +6,9 @@ const userController = require('../controllers/user.controller');
 router.post('/register', userController.register);
 router.post('/verify-email', userController.verifyEmail);
 router.post('/login', userController.login);
-router.get('/users/professionals', userController.getProfessionals);
-
-// Route to get user profile by userId
-router.get('/users/:userId', userController.getUserProfile);
-
-// Route to update user profile
-router.put('/users/update-profile/:userId', userController.updateUserProfile);
 
 // Route to check if user exists
 router.get('/users/exists', userController.checkUserExists); // New route for checking if user exists
 
-// Route for Google OAuth sign-in
-router.post('/auth/google', userController.handleGoogleOAuth); // Route for Google OAuth
 
 module.exports = router;
