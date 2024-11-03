a5c10658dc039726cc2294a35c9b72cd4d04e12e
Your Name <your_email@example.com>
2024-11-04 01:29:59 +0300

    made some changes to the subaccount routes and controllers

--- api/routes/subaccount.routes.js
+++ api/routes/subaccount.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const subaccountController = require('../controllers/sub_account.controller');
 
 // Route to fetch subaccount info by user ID
-router.get('/subaccount/:professionalIds', subaccountController.getSubaccountByProfessionalId);
+router.get('/subaccount/:professionalId', subaccountController.getSubaccountByProfessionalId);
 
 
 module.exports = router;
