034efdf76782b3381f3a68930779516e8b9ea7b6
Your Name <your_email@example.com>
2024-11-04 22:07:09 +0300

    minor changes

--- components/SignInWithOAuth.tsx
+++ components/SignInWithOAuth.tsx
@@ -18,8 +18,7 @@ const SignInWithOAuth: React.FC<SignInWithOAuthProps> = ({ setErrorMessage, rout
     clientId: '@parroti/medplus-app',
     androidClientId: '399287117531-s5ea9q7t3v9auj3tspnvi3j70fd9tdg8.apps.googleusercontent.com',
     webClientId: '399287117531-tmvmbo06a5l8svihhb7c7smqt7iobbs0.apps.googleusercontent.com',
-    // Updated redirectUri to match authorized URI
-    redirectUri: 'http://localhost:8081/auth/google/callback',
+    redirectUri: 'https://auth.expo.io/@parroti/medplus-app', // Use custom scheme
   });
 
   React.useEffect(() => {
