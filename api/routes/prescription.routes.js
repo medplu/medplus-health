09e9007b75db17f7525ab48ff62ebacb7070d847
Your Name <your_email@example.com>
2024-11-06 20:48:35 +0300

    modified the route.js

--- api/routes/prescription.routes.js
+++ api/routes/prescription.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { createPrescription } = require('../controllers/prescription.controller');
 const fileUpload = require('express-fileupload');
-
 const router = express.Router();
+const prescriptionController = require('../controllers/prescription.controller');
 
 router.use(fileUpload());
 
-router.post('/prescriptions', createPrescription);
+router.post('/prescriptions', prescriptionController.createPrescription);
+router.get('/prescriptions/:id/download', (req, res) => {
+  const pdfPath = `./prescriptions/${req.params.id}.pdf`;
+  res.download(pdfPath);
+});
 
 module.exports = router;
