facc308c59f43757e0216739565134f1764414a7
medplu <medpluscollaborate@gmail.com>
2025-01-02 17:13:49 +0300

    Update professional.model.js

--- api/models/professional.model.js
+++ api/models/professional.model.js
@@ -9,17 +9,25 @@ const ProfessionalSchema = new Schema({
   bio: { type: String },
   clinicId: { type: Schema.Types.ObjectId, ref: 'Clinic' },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
-  profileImage: { type: String },
   consultationFee: { type: Number },
-  availability: [{ type: String }],
-  slots: [{
-    day: { type: String },
-    startTime: { type: String },
-    endTime: { type: String },
-    isBooked: { type: Boolean, default: false }
-  }],
-  attachedToClinic: { type: Boolean, default: false }, // Ensure this field exists
-  clinic_images: [{ type: Schema.Types.ObjectId, ref: 'ClinicImage' }] // Add this line
+  attachedToClinic: { type: Boolean, default: false },
+  clinic_images: [{ type: Schema.Types.ObjectId, ref: 'ClinicImage' }],
+
+  // Updated professionalDetails field
+  professionalDetails: {
+    medicalDegrees: [
+      {
+        degree: { type: String },
+        institution: { type: String },
+        year: { type: String },
+      },
+    ],
+    specialization: { type: String },
+    certifications: [{ type: String }],
+    licenseNumber: { type: String },
+    issuingMedicalBoard: { type: String },
+    yearsOfExperience: { type: Number },
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Professional', ProfessionalSchema);
