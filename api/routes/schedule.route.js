237f8856153f358ace0768c2c54fdd8370ccae6a
medplu <medpluscollaborate@gmail.com>
2025-01-30 10:32:15 +0300

    Update schedule.route.js

--- api/routes/schedule.route.js
+++ api/routes/schedule.route.js
@@ -1,14 +1,10 @@
+
 const express = require('express');
 const router = express.Router();
 const scheduleController = require('../controllers/schedule.controller');
 
-
 router.post('/schedule', scheduleController.addSchedule);
-
-// Update a slot
-router.put('/schedule/:id/slot', scheduleController.updateSlot);
-
-// Get schedules
-router.get('/schedules', scheduleController.getSchedules);
+router.put('/schedule', scheduleController.updateSlot);
+router.get('/schedule/:doctorId', scheduleController.getSchedules);
 
 module.exports = router;
